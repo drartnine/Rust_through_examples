3337dfb57d12ae818237037ab85bb6c6e40644ab
Dr ArtNine <drartnine@gmail.com>
2022-01-24 16:43:03 +0800

    3rd commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-64f7b0f69eac9520.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\hello_cargo\target\debug\deps\hello_cargo-64f7b0f69eac9520.rmeta: src\main.rs
+
+C:\Users\Biolector2\Github2022\hello_cargo\target\debug\deps\hello_cargo-64f7b0f69eac9520.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Biolector2\Github2022\hello_cargo\src\main.rs
