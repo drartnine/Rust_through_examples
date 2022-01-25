647191d86160ffcdc3e9524256f9ff2175d56e3f
Dr ArtNine <drartnine@gmail.com>
2022-01-25 23:38:01 +0800

    2nd one Jan 25

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,7 +1,9 @@
+use std::io;
+
 fn main() {
     //data types
     // integer
-    let x =2 ;
+    /*let x =2 ;
     println! ("x is {}", x);
     let x : u8 = 255;
     let y : u32 = 256;
@@ -9,6 +11,53 @@ fn main() {
     // float
 
     let z = 2.0;
-    
+    */
+
+    //tuple
+    /*let tup : (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println! ("the value of y is {}", y); //6.4
+
+    let x0 = tup.0;
+
+    let x1 = tup.1;
+
+    let x2 = tup.2; //1
+
+    println! ("the value of z is {}", x2);
+    */
+
+    //array
+    /*let a = [1,2,3,4,5];
+
+    let b: [i32; 5] = [1,2,3,4,5];
+
+    let c = [3; 5];
+
+    println!("{}",c[0])
+    */
+    let a = [1,2,3,4,5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Fail to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("Index entered was not a number");
+
+        let element = a[index];
+
+        println!(
+            "The value of the element at index {} is: {}",
+        index, element
+    );
 
 }
