e91763fa810e5dcacf13fa2484a6f6d978eaabac
Dr ArtNine <drartnine@gmail.com>
2022-01-26 16:38:01 +0800

    1st one Jan 26

---
+++ functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\Biolector2\Github2022\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\functions\target\debug\functions.exe: C:\Users\Biolector2\Github2022\functions\src\main.rs
