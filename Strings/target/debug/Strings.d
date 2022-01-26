9ef12bfc4abcf3da7d7b08f2e3febff811538271
Dr ArtNine <drartnine@gmail.com>
2022-01-26 22:06:27 +0800

    5th

---
+++ Strings/target/debug/Strings.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\Strings\target\debug\Strings.exe: C:\Users\Biolector2\Github2022\Strings\src\main.rs
---
+++ Strings/target/debug/deps/Strings.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\Strings\target\debug\deps\Strings.exe: src\main.rs
+
+C:\Users\Biolector2\Github2022\Strings\target\debug\deps\Strings.d: src\main.rs
+
+src\main.rs:
