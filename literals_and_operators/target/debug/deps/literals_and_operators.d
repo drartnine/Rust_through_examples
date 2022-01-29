43bbd759ccc8959b0d20fca7cc58c2acaf25dcd9
Dr ArtNine <drartnine@gmail.com>
2022-01-29 21:22:57 +0800

    2nd 29

---
+++ literals_and_operators/target/debug/deps/literals_and_operators.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\literals_and_operators\target\debug\deps\literals_and_operators.exe: src\main.rs
+
+C:\Users\Biolector2\Github2022\literals_and_operators\target\debug\deps\literals_and_operators.d: src\main.rs
+
+src\main.rs:
---
+++ literals_and_operators/target/debug/literals_and_operators.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\literals_and_operators\target\debug\literals_and_operators.exe: C:\Users\Biolector2\Github2022\literals_and_operators\src\main.rs
