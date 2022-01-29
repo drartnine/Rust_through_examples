147a6af2aa462158b37dff9fc080dc15909582fa
Dr ArtNine <drartnine@gmail.com>
2022-01-29 21:58:40 +0800

    5th 29

---
+++ Rust_through_examples/tuples/target/debug/deps/tuples.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\Rust_through_examples\tuples\target\debug\deps\tuples.exe: src\main.rs
+
+C:\Users\Biolector2\Github2022\Rust_through_examples\tuples\target\debug\deps\tuples.d: src\main.rs
+
+src\main.rs:
---
+++ Rust_through_examples/tuples/target/debug/tuples.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\Rust_through_examples\tuples\target\debug\tuples.exe: C:\Users\Biolector2\Github2022\Rust_through_examples\tuples\src\main.rs
