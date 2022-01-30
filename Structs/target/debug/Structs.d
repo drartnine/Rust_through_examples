ca1ce9d95368b54d203b49dd010f0aa024b8de4d
drartnine <98303071+drartnine@users.noreply.github.com>
2022-01-30 16:33:08 +0800

    Delete Structs directory

--- Structs/target/debug/Structs.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Biolector2\Github2022\Structs\target\debug\Structs.exe: C:\Users\Biolector2\Github2022\Structs\src\main.rs
--- Structs/target/debug/deps/Structs.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Biolector2\Github2022\Structs\target\debug\deps\Structs.exe: src\main.rs
-
-C:\Users\Biolector2\Github2022\Structs\target\debug\deps\Structs.d: src\main.rs
-
-src\main.rs:
