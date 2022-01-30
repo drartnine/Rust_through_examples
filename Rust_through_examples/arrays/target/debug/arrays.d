03334977e3d656bb600a941721237509cb50d1b1
Dr ArtNine <drartnine@gmail.com>
2022-01-30 16:31:43 +0800

    2nd 30

---
+++ Rust_through_examples/arrays/target/debug/arrays.d
@@ -0,0 +1,1 @@
+C:\Users\Biolector2\Github2022\Rust_through_examples\arrays\target\debug\arrays.exe: C:\Users\Biolector2\Github2022\Rust_through_examples\arrays\src\main.rs
---
+++ Rust_through_examples/arrays/target/debug/deps/arrays.d
@@ -0,0 +1,5 @@
+C:\Users\Biolector2\Github2022\Rust_through_examples\arrays\target\debug\deps\arrays.exe: src\main.rs
+
+C:\Users\Biolector2\Github2022\Rust_through_examples\arrays\target\debug\deps\arrays.d: src\main.rs
+
+src\main.rs:
