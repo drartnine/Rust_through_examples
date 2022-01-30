a767c804fd25d546702a3f735ffd7e6881c8866c
drartnine <98303071+drartnine@users.noreply.github.com>
2022-01-30 16:32:42 +0800

    Delete Strings directory

--- Strings/target/debug/Strings.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Biolector2\Github2022\Strings\target\debug\Strings.exe: C:\Users\Biolector2\Github2022\Strings\src\main.rs
--- Strings/target/debug/deps/Strings.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Biolector2\Github2022\Strings\target\debug\deps\Strings.exe: src\main.rs
-
-C:\Users\Biolector2\Github2022\Strings\target\debug\deps\Strings.d: src\main.rs
-
-src\main.rs:
